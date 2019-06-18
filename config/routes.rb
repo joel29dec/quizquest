71e9b39102b3ca960811f91f94a791f86d1e66c0
Joel Seng <joel29dec@gmail.com>
2019-06-18 15:09:50 -0500

    checkin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :games, param: :username, only: [:new, :create, :show]
+  resources :games, only: [:index, :new, :create, :show]
   resources :questions, only: [:new, :create, :edit, :update]
   resources :sessions, param: :username, only: [:index, :show, :destroy]
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/games/:game_id/:category_id/:user_id' , to: 'game#show'
 end
