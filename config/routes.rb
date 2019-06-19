acb5a0251b3a9cb94ced9db5a217d613e096d8ab
Joel Seng <joel29dec@gmail.com>
2019-06-19 11:01:54 -0500

    add new login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :games, only: [:index, :new, :create, :show]
   resources :questions, only: [:new, :create, :edit, :update]
-  resources :sessions, param: :username, only: [:index, :show, :destroy]
+  resources :sessions, param: :username, only: [:index, :show, :new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   get '/games/:game_id/:category_id/:user_id' , to: 'game#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
