9f572619836aab56f1cd1ee74f13ef2ab42f3a7f
xmksz3 <markus.sanderst@gmail.com>
2019-06-19 21:57:56 -0500

    FIXED GAME CONTROLLER

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,80 +5,36 @@ class GamesController < ApplicationController
     end
 
 
-     def show
+    def show
        @game = Game.find_by(id: params[:id])
+       @category = Category.find_by(id: params[:id])
+       @current_user = User.find_by(id: session[:user_id])
     end
 
     def lobby
-        #p params
-        @username = params[:username]
+      @username = params[:username]
     end
 
-     def new
-        Game.new
+    def new
+      Game.new
     end
 
     def create
-        # @category = Category.find_by(id: params[:category_id]).questions
-        
-        
-        if params["Create"] == "Start Game"
-          @questions = Question.find_by(category_id: params[:category_id])
-          @game = Game.create(user_id: session[:user_id], category_id: params[:category_id])
-          byebug
-          redirect_to game_path(@game.id)
-        elsif params["Create"] == "Create Game"
-          redirect_to new_game_path
-        end
+
+      if params["Create"] == "Start Game"
+        @questions = Question.find_by(category_id: params[:category_id])
+        category_id = game_params
+        @game = Game.create(user_id: session[:user_id], category_id: 9)
+        redirect_to game_path(@game.id)
+      elsif params["Create"] == "Create Game"
+        redirect_to new_game_path
+      end
 
     end
-
-
-
-
-
-
 
      private
 
-
-
      def game_params
-        params.require(:category).require(:category_id)
-        # params.require(:game).require(:game_id, :game_key)
+        params.require(:category).permit(:category_id)
     end
 end
-
-
-
-
-
-
-
-
-
-
-#   def show
-#         @game = Game.find_by(id: params[:game_id])
-#         @category = @game.category
-#         #@question = @category.questions[1]
-#         # @game = Game.find_by(id: params[:game_id])
-#         # @category = Category.find_by(id: params[:category_id])
-#         # @user = User.find_by(id: params[:user_id])
-#         # @question = Question.find_by
-#         # #@question = Question.find_by(id: params[:category_id].first) #change this out to a random place
-#         # p @question.prompt
-#     end	   
-#      def new
-#         Game.new
-#     end
-#     def create 
-#           @category = Category.find_by(id: params[:category][:category_id].to_i)
-#          p "*******************************"
-#          p @category.questions
-#          p
-#         # @category_questions = @category.questions
-#         p "*******************************"
-#         # p params
-#         #  p @ca÷stions = Question.find_by(category_id: params[:category_id])
-#         # p @questions
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # resources :games, only: [:index, :new, :create, :show]
   # resources :questions, only: [:new, :create, :edit, :update]
   # resources :sessions, param: :username, only: [:index, :show, :new, :create, :destroy]
   # resources :users, only: [:new, :create, :show]
   # get '/games/:id/' , to: 'games#show'
-  # get '/games/:username/lobby', to: 'games#lobby', as: 'lobby'
+
+  resources :games, only: [:index, :new, :create, :show]
+  # get '/game', to: 'games#lobby', as: 'lobby'
 
   resources :sessions, only: [:new, :create]
   resources :users, param: :username, only: [:new, :create, :show]
